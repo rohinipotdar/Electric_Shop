41a336485c52568d5872a3d2e0f5d8ea0a603e41
rohinipotdar <rohinidixit16@gmail.com>
2023-06-12 20:45:17 +0530

    Changes in AppConst

--- src/main/java/com/BikkadIT/ShopElectric/helper/AppConstants.java
+++ src/main/java/com/BikkadIT/ShopElectric/helper/AppConstants.java
@@ -3,4 +3,9 @@ package com.BikkadIT.ShopElectric.helper;
 public class AppConstants {
     public static final String USER_DELETE="User deleted successfully";
     public static final String NOT_FOUND="User not found";
+
+    public static final int page_Num_Default_Value=0;
+
+    public static final int page_Size_Default_Value=2;
+
 }
