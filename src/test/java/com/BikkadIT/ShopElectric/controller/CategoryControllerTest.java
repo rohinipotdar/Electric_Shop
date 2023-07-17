ed5358b6df1c788864995aa71ea6c6a0760aa050
rohinipotdar <rohinidixit16@gmail.com>
2023-07-17 15:42:42 +0530

    added tset cases in category controller

---
+++ src/test/java/com/BikkadIT/ShopElectric/controller/CategoryControllerTest.java
@@ -0,0 +1,120 @@
+package com.BikkadIT.ShopElectric.controller;
+
+import com.BikkadIT.ShopElectric.dtos.CategoryDto;
+import com.BikkadIT.ShopElectric.entities.Category;
+import com.BikkadIT.ShopElectric.entities.Products;
+import com.BikkadIT.ShopElectric.services.CategoryServiceI;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CategoryControllerTest {
+
+    private Category category;
+
+    private Category category1;
+
+    private CategoryDto categoryDto;
+    List<Products> product;
+
+    @MockBean
+    private CategoryServiceI categoryServiceI;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private List<Category> categories;
+
+    @BeforeEach
+    public void init(){
+
+        category =Category.builder()
+                .categoryId("101")
+                .title("mobile phones")
+                .description("all phones are android")
+                .coverImage("mobile123.png")
+                .products(product)
+                .build();
+
+        category1 =Category.builder()
+                .categoryId("102")
+                .title("iphones")
+                .description("all phones are smart phones")
+                .coverImage("iphones.png")
+                .products(product)
+                .build();
+
+        categories = new ArrayList<>();
+        categories.add(category);
+        categories.add(category1);
+
+    }
+
+    @Test
+    void createCategoryTest() throws Exception {
+
+        CategoryDto categoryDto=mapper.map(category,CategoryDto.class);
+
+        Mockito.when(categoryServiceI.createCategory(Mockito.any())).thenReturn(categoryDto);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/categories")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(ConvertObjectToJsonString(category))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.title").exists());
+
+    }
+    private String ConvertObjectToJsonString(Object user){
+        try{
+
+            return new ObjectMapper().writeValueAsString(user);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Test
+    void updateCategory() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void getAllCategory() {
+    }
+
+    @Test
+    void deleteCategory() {
+    }
+
+    @Test
+    void createProductWithCategory() {
+    }
+}
