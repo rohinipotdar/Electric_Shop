97f1b5328abe833811d2e97e29ae9a481e44d016
rohinipotdar <rohinidixit16@gmail.com>
2023-07-23 16:24:16 +0530

    added new cart service in cart module

---
+++ src/main/java/com/BikkadIT/ShopElectric/services/CartServiceI.java
@@ -0,0 +1,17 @@
+package com.BikkadIT.ShopElectric.services;
+
+import com.BikkadIT.ShopElectric.dtos.AddItemToCartRequest;
+import com.BikkadIT.ShopElectric.dtos.CartDtos;
+
+public interface CartServiceI {
+    //add item to cart
+    //case1: cart is not available for user we will create the cart and then add the cart
+    //case2: cart available add the items to CART
+    CartDtos addItemToCart(String userId, AddItemToCartRequest request);
+
+    //remove item from cart
+    void removeItemFromCart(String usetId,int cartItem);
+
+    //remove all items from cart
+    void clearCart(String userId);
+}
