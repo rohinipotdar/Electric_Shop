e1ae5cd9261457a035bea1b0273aa99fc6a439d2
rohinipotdar <rohinidixit16@gmail.com>
2023-07-26 17:02:06 +0530

    added new comments in order module

--- src/main/java/com/BikkadIT/ShopElectric/entities/CreateOrderRequest.java
+++ src/main/java/com/BikkadIT/ShopElectric/entities/CreateOrderRequest.java
@@ -6,7 +6,7 @@ import lombok.*;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
+
 public class CreateOrderRequest {
     private String userId;
 
--- src/main/java/com/BikkadIT/ShopElectric/services/impl/OrderServiceImpl.java
+++ src/main/java/com/BikkadIT/ShopElectric/services/impl/OrderServiceImpl.java
@@ -3,22 +3,41 @@ package com.BikkadIT.ShopElectric.services.impl;
 import com.BikkadIT.ShopElectric.dtos.OrderDtos;
 import com.BikkadIT.ShopElectric.dtos.PageableResponse;
 import com.BikkadIT.ShopElectric.entities.CreateOrderRequest;
+import com.BikkadIT.ShopElectric.repository.CartRepository;
+import com.BikkadIT.ShopElectric.repository.OrderRepository;
+import com.BikkadIT.ShopElectric.repository.UserRepository;
 import com.BikkadIT.ShopElectric.services.OrderServiceI;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderServiceI {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
     @Override
     public OrderDtos createOrder(CreateOrderRequest createOrderRequest) {
-
-
         return null;
     }
 
     @Override
     public Void removeOrder(String orderId) {
+
+
         return null;
     }
 
