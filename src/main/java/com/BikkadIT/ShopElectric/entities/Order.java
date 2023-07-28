559dccce39fa5459abc3b6e26206922155c69345
rohinipotdar <rohinidixit16@gmail.com>
2023-07-28 18:19:04 +0530

    added new fields in order module

--- src/main/java/com/BikkadIT/ShopElectric/entities/Order.java
+++ src/main/java/com/BikkadIT/ShopElectric/entities/Order.java
@@ -2,8 +2,10 @@ package com.BikkadIT.ShopElectric.entities;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Setter
@@ -15,4 +17,29 @@ import javax.persistence.Table;
 public class Order {
 
     private String orderId;
+
+    private String orderStatus;
+
+    private String paymentStatus;
+
+    private int orderAmount;
+
+    @Column(length = 1000)
+    private String billingAddress;
+
+    private String billingPhone;
+
+    private String billingName;
+
+    private Date orderDate;
+
+    private Date DeliveryDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "",fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems= new ArrayList<>();
+
 }
--- src/main/java/com/BikkadIT/ShopElectric/repository/OrderRepository.java
+++ src/main/java/com/BikkadIT/ShopElectric/repository/OrderRepository.java
@@ -1,9 +1,15 @@
 package com.BikkadIT.ShopElectric.repository;
 
 import com.BikkadIT.ShopElectric.entities.Order;
+import com.BikkadIT.ShopElectric.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,String> {
+
+    List<Order> findByUser(User user);
+
 }
